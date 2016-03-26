206dc655de9a51706dc9344547e79d4fa4cedaab
Allan Berry <allan.berry@gmail.com>
2016-03-26 02:28:56 -0500

    Swap out bower for npm.

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask( __name__,
+        static_folder="static",
+    )
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+    )
