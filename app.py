d295781b00a6e012fb95097c9dcb5c54fd23e9fb
Allan Berry <allan.berry@gmail.com>
2016-03-26 15:43:52 -0500

    Basic file structure in place.

--- app.py
+++ app.py
@@ -6,8 +6,31 @@ app = Flask( __name__,
     )
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route("/cars")
+def show_cars():
+    return render_template('cars.html',)
+
+@app.route("/cars/<marque>")
+def show_marque(marque):
+    context = {
+        'marque': marque,
+    }
+    return render_template('marque.html', context=context)
+
+@app.route("/cars/<marque>/<model>")
+def show_model(marque, model):
+    context = {
+        'marque': marque,
+        'model': model,
+    }
+    return render_template('model.html', context=context)
+
+@app.route("/colophon")
+def colophon():
+    return render_template('colophon.html')
 
 if __name__ == "__main__":
     app.run(
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Cars
+
+This is an attempt to explore some new technologies.  It's definitely a work in progress, and will take some time to mature.
+
+Thanks for visiting.
